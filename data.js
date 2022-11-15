b931e98390b17f9b0872672070107b1a26646bdb
MrFranksJr <frankyjr@me.com>
2022-11-15 18:33:30 +0100

    First commit

---
+++ data.js
@@ -0,0 +1,71 @@
+export const mainsArray = [
+    {
+        name: "Ben Ben Noodles",
+        description: `Fresh Shitake mushrooms and baby pak choi, rubbed alongside tahini and chili oil 😍`,
+        id: 0,
+        price: "27",
+        image: "./images/benben.png"
+    },
+    {
+        name: "The Frankito Burger",
+        description: `A fresh, juicy slab of meat, grilled rare and packed between locally sourced bread and doused in secret sauce... Comes with crispy, fresh fries`,
+        id: 1,
+        price: "16",
+        image: "./images/burger.png"
+    },
+        {
+        name: "Salmon Linguine",
+        description: `Straight from Vancouver Island onto your plate, together with perfectly cooked, al dente linguine from my Italian grandmother. Try not to gobble it all at once!`,
+        price: "18",
+        image: "./images/salmon.png",
+        id: 2
+    }
+]
+
+export const drinksArray = [
+    {
+        name: "Edelpils",
+        description: `Delicious German Pilsner from the heart of Bayern`,
+        price: "3,5",
+        image: "./images/edelpils.png",
+        id: 0
+    },
+    {
+        name: "Coca-Cola",
+        description: `Your classic sugary drink, served ice cold!`,
+        price: "2,5",
+        image: "./images/cola.png",
+        id: 1
+    },
+    {
+        name: "Club Mate",
+        description: `Short on caffeine? Hold my Mate...`,
+        price: "4",
+        image: "./images/mate.png",
+        id: 2
+    }
+]
+
+export const dessertsArray = [
+    {
+        name: "Pizza",
+        ingredients: ["pepperoni", "mushrom", "mozarella"],
+        id: 0,
+        price: 14,
+        emoji: "🍕"
+    },
+    {
+        name: "Hamburger",
+        ingredients: ["beef", "cheese", "lettuce"],
+        price: 12,
+        emoji: "🍔",
+        id: 1
+    },
+        {
+        name: "Beer",
+        ingredients: ["grain, hops, yeast, water"],
+        price: 12,
+        emoji: "🍺",
+        id: 2
+    }
+]
---
+++ index.js
@@ -0,0 +1,76 @@
+//import data
+import { mainsArray } from '/data.js'
+import { drinksArray } from '/data.js'
+import { dessertsArray } from '/data.js'
+
+const drinksSection = document.getElementById("drinks-menu-items")
+const mainsSection = document.getElementById("main-menu-items")
+const dessertsSection = document.getElementById("desserts-menu-items")
+
+function collectDrinks() {
+    let drinksHTMLcontent = ''
+    for (const drink of drinksArray) {
+        drinksHTMLcontent += `
+            <div class="menu-item">
+                <div class="col-1">
+                    <img src="${drink.image}" class="food-image">
+                </div>
+                <div class="col-2">
+                    <h3>${drink.name}</h3>
+                    <p class="food-description">${drink.description}</p>
+                    <div class="price">$${drink.price}</div>
+                    <button class="order-button">ADD TO ORDER</button>
+                </div>
+            </div>
+        `
+    }
+    return drinksHTMLcontent
+}
+
+function collectMains() {
+    let mainsHTMLcontent = ''
+    for (const main of mainsArray) {
+        mainsHTMLcontent += `
+            <div class="menu-item">
+                <div class="col-1">
+                    <img src="${main.image}" class="food-image">
+                </div>
+                <div class="col-2">
+                    <h3>${main.name}</h3>
+                    <p class="food-description">${main.description}</p>
+                    <div class="price">$${main.price}</div>
+                    <button class="order-button">ADD TO ORDER</button>
+                </div>
+            </div>
+        `
+    }
+    return mainsHTMLcontent
+}
+
+function collectDesserts() {
+    let dessertsHTMLcontent = ''
+    for (const dessert of dessertsArray) {
+        dessertsHTMLcontent += `
+            <div class="menu-item">
+                <div class="col-1">
+                    <img src="${dessert.image}" class="food-image">
+                </div>
+                <div class="col-2">
+                    <h3>${dessert.name}</h3>
+                    <p class="food-description">${dessert.description}</p>
+                    <div class="price">$${dessert.price}</div>
+                    <button class="order-button">ADD TO ORDER</button>
+                </div>
+            </div>
+        `
+    }
+    return dessertsHTMLcontent
+}
+
+function renderFood() { 
+    drinksSection.innerHTML = collectDrinks()
+    mainsSection.innerHTML = collectMains()
+    dessertsSection.innerHTML = collectDesserts()
+}
+
+renderFood()
